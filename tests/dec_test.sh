bb529b4f9975b73a6ea534f504261f21ef6ae6fa
alfrentgen <alfrentgen@gmail.com>
2018-01-09 10:07:54 +0700

    tests were modified

--- tests/dec_test.sh
+++ tests/dec_test.sh
@@ -1,4 +1,3 @@
 date
-echo $3
-../build/qvsdec -f $1 -i ./$2.yuv -o ./$2.dec -p 99 -w $3 -m mixed -k #2>/dev/null
+../build/qvsdec -f $1 -i ./$2.yuv -o ./$2.dec -p 99 -w $3 -m mixed #2>/dev/null
 date
--- tests/mtenc_test.sh
+++ tests/mtenc_test.sh
@@ -1,3 +1,3 @@
 date
-../build/mtqvsenc -f $1 -s 4 -i ./$2.bin -o ./$2_$1_mt.yuv -w 2 -r 4 -t 10
+../build/mtqvsenc -f $1 -s 8 -i ./$2.bin -o ./$2_$1_mt.yuv -w 2 -p 99
 date
