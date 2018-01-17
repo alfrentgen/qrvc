c857b63cb18458487140c348a71d69b36913db6e
alf <alf@alf.com>
2018-01-17 19:36:47 +0700

    todo list has been updated.

--- tests/mtenc_ff_test.sh
+++ tests/mtenc_ff_test.sh
@@ -1,3 +1,3 @@
 date
-../build/mtqvsenc -f $1 -s 8 -i ./$2.bin -w 2 -r 1 -t 10 -p 99 | ffmpeg -y -f rawvideo -pix_fmt gray -s:v $1 -i - -c:v libx264 -x264opts keyint=1 $2_$1.avi
+../build/mtqvsenc -f $1 -s 8 -i ./$2 -w $3 -r 1 -t 10 -p 99 | ffmpeg -y -f rawvideo -pix_fmt gray -s:v $1 -i - -c:v libx264 -x264opts keyint=1 $2_$1.avi
 date
