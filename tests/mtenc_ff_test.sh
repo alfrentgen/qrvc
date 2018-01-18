8e886065f4ca2bc0537ecb1cff49869ad3d4f4ef
alf <alf@alf.com>
2018-01-18 13:02:00 +0700

    test scripts were modified.

--- tests/mtenc_ff_test.sh
+++ tests/mtenc_ff_test.sh
@@ -1,3 +1,3 @@
 date
-../build/mtqvsenc -f $1 -s 8 -i ./$2 -w $3 -a $4 -r 1 -t 10 -p 99 | ffmpeg -y -f rawvideo -pix_fmt gray -s:v $1 -i - -c:v libx264 -preset fast -x264-params subme=0:me="dia"  $2_$1.avi
+../build/mtqvsenc -f $1 -s 4 -i ./$2 -w $3 -a $4 -r 1 -t 10 -p 99 | ffmpeg -y -f rawvideo -pix_fmt gray -s:v $1 -i - -c:v libx264 -preset fast -x264-params subme=0:me="dia"  $2_$1.avi
 date
