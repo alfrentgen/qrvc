0aacd78e91d5e68dda17af4b811bf0eac27fdcef
alf <alf@alf.com>
2018-03-10 00:33:11 +0700

    some tests were modified.

--- tests/encdec_rnd_test.sh
+++ tests/encdec_rnd_test.sh
@@ -1,5 +1,4 @@
+head -c $1 < /dev/urandom > $1.bin
 date
-head -c $2 < /dev/urandom > $2.bin
-../build/qvsenc -i $2.bin -f $1 -s 4 |
-../build/qvsdec -f $1 -o $2.dec -p 99 -w 3 -m quick
+../build/qrve -i $1.bin -p 99 | ../build/qrvd -o $1.dec -p 99
 date
--- tests/encdec_test.sh
+++ tests/encdec_test.sh
@@ -1,7 +1,7 @@
 date
 #../build/qvsenc -f $1 -s 8 -w 2 -i ./$2.bin -r 1 -t 10 -p 99 | ../build/qvsdec -f $1 -p 99 -w 2 -m mixed -o ./$2.dec
 #test auto params
-../build/qvsenc -i ./$1.bin -p 99 | ../build/qvsdec -o ./$1.dec -p 99
+../build/qrve -i ./$1.bin -p 99 | ../build/qrvd -o ./$1.dec -p 99
 date
 #sha512sum $2.bin $2.dec
 sha512sum $1.bin $1.dec
