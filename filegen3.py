5fd9b21993bd9deb24c43659ba750aec52216c8a
alf <alf@alf.com>
2018-05-10 19:05:01 +0700

    minor changes in filegen3

--- filegen3.py
+++ filegen3.py
@@ -1,18 +1,18 @@
-import  os
-working_path = '/media/alf/storage1/qrvc/qrvc_linux_x86_64'
-os.chdir(working_path)
+#import  os
+#working_path = '/media/alf/storage1/qrvc/qrvc_linux_x86_64'
+#os.chdir(working_path)
 
 dest = open('sample.yuv', 'wb')
 qr = open('1M_1280x720.yuv', 'rb')
-anim = open('anim.yuv', 'rb')
-anim_frame = 210
+anim = open('BAO.yuv', 'rb')
+anim_frame = 66
 threshold = 0x7fffff
 width = int(1280)
 height = int(720)
 
 chroma_size = int(width * height/4)
-luma_size = chroma_size * 4
-frame_size = chroma_size + 2 * luma_size
+luma_size = int(chroma_size * 4)
+frame_size = int(chroma_size + 2 * luma_size)
 
 qr_luma = qr.read(luma_size)
 qr_luma = bytearray(qr_luma)
@@ -30,23 +30,38 @@ anim.seek(anim_offset + luma_size + chroma_size)
 anim_v = anim.read(chroma_size)
 anim_v = bytearray(anim_v)
 
-for i in range(luma_size):
-    if anim_y[i] in range(64, 128):
-        if qr_luma[i] == 0:
-            anim_y[i] = 63
-        elif qr_luma[i] == 255:
-            anim_y[i] = 127
+plane = anim_v
+
+try:
+    for i in range(luma_size):
+        lum_raw = int(i/width)
+        lum_col = int(i%width)
+        ch_raw = int((i/width)/2)
+        ch_col = int((i%width)/2)
+        ch_width = int(width/2)
+        ch_i = int(ch_raw * ch_width + ch_col)
+
+        if plane[ch_i] in range(64, 128):
+            if qr_luma[i] == 0:
+                plane[ch_i] -= 64
+            elif qr_luma[i] == 255:
+                plane[ch_i] += 64
             
-    if anim_y[i] in range(128, 128 + 64):
-        if qr_luma[i] == 0:
-            anim_y[i] = 127
-        elif qr_luma[i] == 255:
-            anim_y[i] = 128 + 64
+        if plane[ch_i] in range(128, 128 + 64):
+            if qr_luma[i] == 0:
+                plane[ch_i] -= 64
+            elif qr_luma[i] == 255:
+                plane[ch_i] += 64
+except Exception as ex:
+    print("exception")
+finally:
+    print(len(anim_y), len(anim_u),len(anim_v))
+    print(i, lum_raw, lum_col)
+    print(ch_i, ch_raw, ch_col)
+    
+    synth_frame = anim_y + anim_u + anim_v
 
-print(len(anim_y), len(anim_u),len(anim_v))
-synth_frame = anim_y + anim_u + anim_v
-
-dest.write(synth_frame)
-dest.close()
-qr.close()
-anim.close()
+    dest.write(synth_frame)
+    dest.close()
+    qr.close()
+    anim.close()
