214c25bf4fa658135c48b446348a4daaf21a497c
alf <alf@alf.com>
2018-05-24 20:47:44 +0700

    working out a path algorithm

--- include/steganography.h
+++ include/steganography.h
@@ -2,6 +2,7 @@
 #define STEGANOGRAPHY_H
 #include <pch.h>
 
-
+int32_t Hide(uint8_t* frame, int32_t width, int32_t height, uint8_t* qrCode, int32_t qrwWidth);
+int32_t nextQRPointIdx(int32_t currentIdx, int32_t qrWidth);
 
 #endif
--- src/steganography.cpp
+++ src/steganography.cpp
@@ -1,1 +1,9 @@
 #include "steganography.h"
+
+int32_t spiralMatrix(int32_t currentIdx, int32_t qrWidth){
+    //currentIdx
+}
+
+int32_t nextQRPointIdx(int32_t currentIdx, int32_t qrWidth){
+    spiralMatrix(int32_t currentIdx, int32_t qrWidth);
+}
