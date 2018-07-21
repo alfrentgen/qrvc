d42909f14cc2df2093137119cc547cadb062f832
alfrentgen <alfrentgen@gmail.com>
2018-07-22 01:40:42 +0700

    steg_test.sh was improved!

--- tests/steg_test.sh
+++ tests/steg_test.sh
@@ -1,10 +1,22 @@
 IN_FILE=$1
-OUT_FILE=$2
-THRESHOLD=$3
+YUV_FILE=$IN_FILE.yuv
+KEY_FILE=$IN_FILE.stg
+MP4_FILE=$IN_FILE.mp4
+MP4_YUV=$MP4_FILE.yuv
+OUT_FILE=$IN_FILE.dec
+THRESHOLD=$2
 
-rm 1M_stg.yuv 1M.in.stg 1M_stg.mp4 1M_stg.mp4.yuv $2 $1.stg
-ffmpeg -i short.mp4 -f rawvideo -pix_fmt yuv420p - 2>/dev/null | ./qrve -i $1 -o 1M_stg.yuv --stg th=$3
-ffmpeg -y -f rawvideo -pix_fmt yuv420p -s:v 1280x720 -i 1M_stg.yuv -c:v libx264 -pix_fmt yuv420p 1M_stg.mp4 2>/dev/null
-ffmpeg -i 1M_stg.mp4 -f rawvideo -pix_fmt yuv420p 1M_stg.mp4.yuv 2>/dev/null
-./qrvd -i 1M_stg.mp4.yuv --stg kf=$1.stg -o $2
-sha512sum $1 $2
+if [ -z "$3" ]
+then
+      HOST_FILE=host.mp4
+else
+      HOST_FILE=$3
+fi
+
+rm $IN_FILE $YUV_FILE $KEY_FILE $MP4_FILE $MP4_YUV $OUT_FILE
+head -c 1M < /dev/urandom > $IN_FILE
+ffmpeg -i $HOST_FILE -f rawvideo -pix_fmt yuv420p - 2>/dev/null | ./qrve -i $IN_FILE -o $YUV_FILE --stg th=$THRESHOLD
+ffmpeg -y -f rawvideo -pix_fmt yuv420p -s:v 1280x720 -i $YUV_FILE -c:v libx264 -pix_fmt yuv420p $MP4_FILE 2>/dev/null
+ffmpeg -i $MP4_FILE -f rawvideo -pix_fmt yuv420p $MP4_YUV 2>/dev/null
+./qrvd -i $MP4_YUV --stg kf=$KEY_FILE -o $OUT_FILE
+sha512sum $IN_FILE $OUT_FILE
